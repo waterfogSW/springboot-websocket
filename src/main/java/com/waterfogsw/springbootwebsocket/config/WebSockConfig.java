068b10318f9360fd33b59daec7a50e04d4d4f6db
San Kim <waterfog9580@gmail.com>
2022-06-25 22:08:40 +0900

    :sparkles: WebSocket 설정 추가

---
+++ src/main/java/com/waterfogsw/springbootwebsocket/config/WebSockConfig.java
@@ -0,0 +1,22 @@
+package com.waterfogsw.springbootwebsocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSockConfig implements WebSocketConfigurer {
+  private final WebSocketHandler webSocketHandler;
+
+  public WebSockConfig(WebSocketHandler webSocketHandler) {
+    this.webSocketHandler = webSocketHandler;
+  }
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+  }
+}
